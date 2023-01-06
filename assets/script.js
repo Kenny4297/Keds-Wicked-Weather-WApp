706ca80af47eb325ca6826ae6feb4df04a737f10
Kenny Cordero <geckob4i@gmail.com>
2023-01-06 07:45:41 -0600

    Tried coding in bed. Not a good idea. made a few changes in CSS

--- assets/script.js
+++ assets/script.js
@@ -59,7 +59,6 @@ const historyToInput = (event) => {
 historyOption.addEventListener('click', historyToInput)
 
 
-
 //Get the current day's forecast
 const returnCurrentForecast = async (event) => {
     event.preventDefault();
